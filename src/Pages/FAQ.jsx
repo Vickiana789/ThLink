a8f443d63ec9bddb363414b838043d27ceaf9858
codeathon47 <codeathon47@wpsd.k12.pa.us>
2018-01-28 08:13:06 -0800

    updates

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -6,6 +6,8 @@ export default () => (
         <p><ul>
             <li>71 million teenagers do not finish secondary school due to a lack of funding</li>
             <li>Over 180 million children did not go through primary school</li>
+            <li> India also faces shortages in excess of 350,000, while Indonesia needs nearly 190,000 more teachers. </li>
+            <li> Teacher shortages is ranked as the second biggest factor influencing academic and administrative operations. </li>
 
                 </ul>
             </p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,11 +10,12 @@ export default () => (
 
         <body>
             <h2>What We Are</h2>
-        ThLink is a foundation built on revitalizing education in areas where they are non existent because of a problem specialized by our organization which is the shortage of teachers.
+            <p>ThLink is a foundation built on revitalizing education in areas where they are non existent because of a problem specialized by our organization which is the shortage of teachers.
  Why teachers ?   Our competitors believe that the only way  to improve a society is by increasing the amount of buildings in the area or donating money. Although these things are helpful we dcommunity and attempt to change so that it can be reformed  for our terms of the better we educate the students so they can be enlightened and grow their culture themselves. This doesn't only stand to benefit students as we also increase job opportunities  for educators. Our developers created a systematic cycle in which the teacher is offered a fair living in the area where they are resigned to teach  and  provisions if they can complete levels of tedious online classes which have vital educational and cultural knowledge. Help yourself help others the
-improving the bond between students and upcoming teachers.
+improving the bond between students and upcoming teachers.</p>
 
 <h2>The Problem</h2>
+<p>To understand the scale of the situation, over 74 countries are struggling with a acute shortage of teachers. Over 180 million children did not go through primary school, and 71 million teenager did not finish secondary school due to a lack of funding. A massive problem that requires a viable solution. We need to take charge!</p>
 
         </body>
     </div>
