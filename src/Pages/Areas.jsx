1f993d49f2de10bf40ee6f002c66a462c31b4f80
codeathon47 <codeathon47@wpsd.k12.pa.us>
2018-01-28 07:27:33 -0800

    update

--- src/Pages/Areas.jsx
+++ src/Pages/Areas.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import worldmap from "../worldmap.png"
 export default () => (
-    <div>
+    <div class="areasofneed">
         <h1>Areas In Need</h1>
         <p>We provide services to teachers when they travel to their teaching areas in the locations of West Africa and India.
-        Their over 50 site that teachers can apply to reside in. </p>
+        Their over 50 site that teachers can apply to reside in.
+
+         </p>
+<img src={worldmap} />
     </div>
 );
