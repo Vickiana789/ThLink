a650a77d48fd6b9374c053ff89bd8f178a1e0a41
codeathon47 <codeathon47@wpsd.k12.pa.us>
2018-01-28 08:44:29 -0800

    TeachHouse Img

--- src/Pages/Provisions.jsx
+++ src/Pages/Provisions.jsx
@@ -4,6 +4,9 @@ export default () => (
     <div>
         <h1>Provisions</h1>
         <p>We provide Teachers with a free online education ( link on requirement page)<br/>.We provide payment for all fees in the area they reside in, including  fees like bills and food.  
+
 </p>
+
+<img src="TeachHouse.jpg"/>
     </div>
 );
